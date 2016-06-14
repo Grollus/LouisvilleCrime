efa7dfe5741e068f2a1779681ef6b0e00e3b355a
Grollus <athoul01@gmail.com>
2016-06-14 11:05:19 -0400

    -Minor readability changes

--- Moran_i_test_for_spatial_correlation.R
+++ Moran_i_test_for_spatial_correlation.R
@@ -2,7 +2,18 @@ library(ggmap)
 library(dplyr)
 library(ape)
 library(readr)
-# Geocode all louisville zip codes
+
+# Read in raw data
+raw_data <- read_csv("crime_lou_with_geocoding.csv")
+
+# Scrape the official list of Louisville zip codes (does not include 
+# the 'nonstandard' zip codes i.e. PO Box or business zip codes)
+library(rvest)
+html <- read_html("http://kentucky.hometownlocator.com/zip-codes/zipcodes,city,louisville.cfm")
+zips <- html_nodes(html, "tr:nth-child(3) a")
+zips <- html_text(zips)
+
+# Geocode all Louisville zip codes 
 zip_cords <- data.frame(zips = factor(), lng = numeric(), lat = numeric())
 for(z in zips){
   temp_cords <- geocode(z, source = 'google')
@@ -10,7 +21,7 @@ for(z in zips){
   zip_cords <- rbind(zip_cords, temp_df)
 }
 
-# Group crimes by zip code and aggregate counts. Then add geocoded lat/lng
+# Function creates useful date variables
 create_date_variables <- function(df){
   require(lubridate)
   # Uses the POSIXct date_occured variable to create useful date related
@@ -29,31 +40,24 @@ create_date_variables <- function(df){
   return(df)
 }
 
-raw_data <- read_csv("crime_lou_with_geocoding.csv")
 crime <- create_date_variables(raw_data)
 
-# Grab the official list of Louisville zip codes(excludes PO boxes and zip codes
-# that are, essentially, companies) and filter to those records
-library(rvest)
-html <- read_html("http://kentucky.hometownlocator.com/zip-codes/zipcodes,city,louisville.cfm")
-zips <- html_nodes(html, "tr:nth-child(3) a")
-zips <- html_text(zips)
-
-crime_zip_aggre <- crime%>%
+# Group crimes by zip code and aggregate counts. Then add geocoded lat/lng
+crime_zip_agg <- crime%>%
   filter(year >=2005 & zip_code %in% zips)%>%
   group_by(zips = factor(zip_code))%>%
   summarise(count = n())
 
-crime_zip_aggre <- left_join(crime_zip_aggre, zip_cords, by = 'zips')
+crime_zip_agg <- left_join(crime_zip_agg, zip_cords, by = 'zips')
 
-#Moran i test for spatial autocorrelation; distance matrix must be generated first
-zip_dists <- as.matrix(dist(cbind(crime_zip_aggre$lon, crime_zip_aggre$lat)))
-#Take inverst and replace diag entries with zero
+#Moran I test for spatial autocorrelation; distance matrix must be generated first
+zip_dists <- as.matrix(dist(cbind(crime_zip_agg$lon, crime_zip_agg$lat)))
+#Take inverse and replace diag entries with zero
 zip_dists_inv <- 1/zip_dists
 diag(zip_dists_inv)<- 0
 
-# Now we can calculate moran i statistic
-Moran.I(crime_zip_aggre$count, zip_dists_inv)
+# Now we can calculate moran I statistic
+Moran.I(crime_zip_agg$count, zip_dists_inv)
 
 # p value is approx 1.242739e-11 indicating spatial autocorrelation
 # This is as expected, but indicates that trying to model this data by time alone
